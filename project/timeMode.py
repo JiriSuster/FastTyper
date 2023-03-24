96115c34200b6ac9272d9cb01b52ec2f723e58c4
David Krcmar <xkrcmar2@mendelu.cz>
2023-03-24 17:37:11 +0100

    typing correction

--- project/timeMode.py
+++ project/timeMode.py
@@ -1,18 +1,30 @@
 from js import document
 from pyodide import create_proxy
 import random
+import time
 
 import funcTimeMode.timeFunc
 import funcCommon.commonFunc
+
 DISPLAY_TEXT = document.getElementById("txt")
+OLD_INPUT_TEXT = ""
 DATA = ""
+DEFAULT_TIME = 60
 TIME = 60
+CURSOR_INDEX = 0
 listOfWords = ["from", "add", "call", "another"]
 
 def setTime(seconds):
-    global TIME
+    global TIME, DEFAULT_TIME
+    DEFAULT_TIME = seconds
     TIME = seconds
     document.getElementById("time").innerHTML = TIME
+
+def setText():
+    global DATA
+    DATA = getRandomText(25)
+    displayText()
+
 def getRandomText(wordsAmount):
     text = ''
     for i in range(wordsAmount-1):
@@ -20,10 +32,9 @@ def getRandomText(wordsAmount):
     text += random.choice(listOfWords)
     return text
 def checkShortcuts(event):
-    global DATA
     if event.keyCode == 27:
-        DATA = getRandomText(20)
-        displayText()
+        setTime(DEFAULT_TIME)
+        setText()
 
 def displayText():
     DISPLAY_TEXT.innerHTML = ""
@@ -31,14 +42,42 @@ def displayText():
         characterSpan = document.createElement("span")
         characterSpan.innerHTML = character
         DISPLAY_TEXT.appendChild(characterSpan)
+
+def changeText(input_value):
+    array_text = DISPLAY_TEXT.querySelectorAll('span')
+
+
+    input_text = OLD_INPUT_TEXT + input_value
+    user_list_input = [char for char in input_text]
+
+    for index, element in enumerate(array_text):
+        if len(user_list_input) <= index:
+            element.classList.remove("correct")
+            element.classList.remove("wrong")
+        else:
+            user_character = user_list_input[index]
+            if element.innerText == user_character:
+                element.classList.remove("wrong")
+                element.classList.add("correct")
+            elif element.innerText != user_character:
+                element.classList.add("wrong")
+                element.classList.remove("correct")
+
+
 def inputEvent(event):
-    if " " in str(event.target.value):
-         event.target.value = ""
+    global OLD_INPUT_TEXT, CURSOR_INDEX
+    value = event.target.value
+    if len(value) > len(DATA.split(" ")[CURSOR_INDEX]):
+        event.target.value = value[:len(DATA.split(" ")[CURSOR_INDEX])]
+    changeText(value)
+    if " " in str(value):
+        OLD_INPUT_TEXT += value
+        CURSOR_INDEX += 1
+        event.target.value = ""
 
 def setup():
-    global DATA
-    DATA = getRandomText(20)
-    displayText()
+    setText()
+    setTime(TIME)
 
     click_proxy = create_proxy(inputEvent)
     click = document.getElementById("text_input")
@@ -46,9 +85,6 @@ def setup():
 
     check_proxy = create_proxy(checkShortcuts)
     document.addEventListener("keydown", check_proxy)
-
-    #setTime
-    document.getElementById("time").innerHTML = TIME
 
 def main():
     setup()
